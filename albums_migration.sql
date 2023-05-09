293413236436f7c281e3a3bb40a8456cf601542b
Daniel <daniel.h.farias2@gmail.com>
2023-05-09 11:19:37 -0500

    tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+Use codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE IF NOT EXISTS albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(128) DEFAULT 'UNKNOWN',
+    name VARCHAR(128),
+    release_date SMALLINT UNSIGNED,
+    sales FLOAT,
+    genre VARCHAR(128),
+    PRIMARY KEY (id)
+    );
